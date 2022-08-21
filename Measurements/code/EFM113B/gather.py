788cd379a6d859d1f783a2adf5a0fd8dcdfc4cd0
jwgit2 <johannwerklegit@gmail.com>
2022-08-21 11:44:38 +0200

    AD recoded

---
+++ Measurements/code/EFM113B/gather.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+
+import time
+from datetime import datetime
+import EFM113B.ADS1263 as ADS1263
+import RPi.GPIO as GPIO
+
+REF = 5.08          # Modify according to actual voltage
+                    # external AVDD and AVSS(Default), or internal 2.5V
+ADC = ADS1263.ADS1263()
+#range = 5           # Range in kV/m
+#resistor = 2.2      # Resistor in kOhm
+
+def AD_init():
+    if (ADC.ADS1263_init_ADC1('ADS1263_7200SPS') == -1):
+        exit()
+    ADC.ADS1263_SetMode(0)
+
+def AD_gather ():
+    try:
+        ADC_Value = ADC.ADS1263_GetChannalValue(0)    # get ADC1 value
+        ADC_0 = REF*2 - ADC_Value * REF / 0x80000000
+        ADC_n0 = (ADC_Value * REF / 0x7fffffff)
+        if(ADC_Value>>31 ==1):
+            print(-ADC_0)
+            return -ADC_0
+        else:
+            print(ADC_n0)
+            return ADC_n0
+
+    except IOError as e:
+        print(e)
+        return 0
+
+def AD_stop():
+    ADC.ADS1263_Exit()
---
+++ Measurements/ina219/ina219_get.py
@@ -0,0 +1,28 @@
+import time
+import board
+from adafruit_ina219 import ADCResolution, BusVoltageRange, INA219
+
+
+def get_battery_voltage():
+    i2c_bus = board.I2C()
+    ina219 = INA219(i2c_bus)
+    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c_bus)
+
+    # optional : change configuration to use 32 samples averaging for both bus voltage and shunt voltage
+    ina219.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+    ina219.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+    # optional : change voltage range to 16V
+    ina219.bus_voltage_range = BusVoltageRange.RANGE_16V
+
+    bus_voltage = ina219.bus_voltage  # voltage on V- (load side)
+    shunt_voltage = ina219.shunt_voltage  # voltage between V+ and V- across the shunt
+    current = ina219.current  # current in mA
+
+    # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
+    #print("Voltage (VIN+) : {:6.3f}   V".format(bus_voltage + shunt_voltage))
+    # Check internal calculations haven't overflowed (doesn't detect ADC overflows)
+    if ina219.overflow:
+        print("Internal Math Overflow Detected!")
+    return (bus_voltage + shunt_voltage)
+
+get_battery_voltage()
